26fd31d1335a78a559b2e4677ba3e977bccfb265
mnunnari@student.42.us.org <mnunnari@e3r2p21.42.fr>
2017-09-18 21:57:11 +0200

    st

--- srcs/ops/st.c
+++ srcs/ops/st.c
@@ -4,11 +4,18 @@ void		st(t_vm *vm, t_process *process, t_op *op, int *args)
 {
 	int		reg;
 	int		idx;
+	int		val;
 
-	(void)op;
 	reg = args[0];
+	val = get_reg_val(process, reg);
 	if (!is_reg_valid(reg))
 		return ;
-	idx = args[1];
-	set_uint32(vm, process->pc + (idx % IDX_MOD), get_reg_val(process, reg));
+	if (op->args_types[1] == T_IND) {
+		idx = args[1];
+		set_uint32(vm, process->pc + (idx % IDX_MOD), val);
+	} else if (op->args_types[1] == T_REG) {
+		reg = args[1];
+		if (is_reg_valid(reg))
+			set_reg_val(process, reg, val);
+	}
 }
