66bbddd85d1e18a0033098720d32d6c31cdee0a2
mnunnari@student.42.us.org <mnunnari@e3r8p15.42.fr>
2017-09-20 22:18:13 +0200

    sti

--- srcs/ops/sti.c
+++ srcs/ops/sti.c
@@ -2,15 +2,30 @@
 
 void		sti(t_vm *vm, t_process *process, t_op *op, int *args)
 {
-	//sti r1, %:live, %1
 	int		reg;
+	int		idx1;
+	int		idx2;
 	int		idx;
 
-	(void)op;
 	reg = args[0];
 	if (!is_reg_valid(reg))
 		return ;
-	idx = args[1] + args[2];
+	idx1 = 0;
+	if (op->args_types[1] == T_DIR)
+		idx1 = args[1];
+	else if (op->args_types[1] == T_IND)
+		idx1 = get_uint32_at(vm, process->pc + args[1]);
+	else if (op->args_types[1] == T_REG)
+	{
+		if (!is_reg_valid(args[1]))
+			return ;
+		idx1 = get_reg_val(process, args[1]);
+	}
+	idx2 = 0;
+	if (op->args_types[2] == T_DIR)
+		idx2 = args[2];
+	else if (op->args_types[2] == T_IND)
+		idx2 = get_uint32_at(vm, process->pc + args[2]);
+	idx = idx1 + idx2;
 	set_uint32(vm, process->pc + (idx % IDX_MOD), get_reg_val(process, reg));
-
 }
